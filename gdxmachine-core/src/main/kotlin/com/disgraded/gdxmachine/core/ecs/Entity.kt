671df42d7bc96fed43cf08a382db7b731015ceed
Daniel Kalevski <kalevski@outlook.com>
2019-08-30 01:24:30 +0200

    entity group changed to optional parameter

--- gdxmachine-core/src/main/kotlin/com/disgraded/gdxmachine/core/ecs/Entity.kt
+++ gdxmachine-core/src/main/kotlin/com/disgraded/gdxmachine/core/ecs/Entity.kt
@@ -3,7 +3,7 @@ package com.disgraded.gdxmachine.core.ecs
 import com.disgraded.gdxmachine.core.engine.Game
 import com.badlogic.ashley.core.Entity as GdxEntity
 
-abstract class Entity(val group: String?) : GdxEntity() {
+abstract class Entity(val group: String? = null) : GdxEntity() {
 
     abstract fun initialize(game: Game)
 
